90f3ee311dba7ca87f6d3f9eb03a7d18071a4c0f
sanved77 <sanved77@gmail.com>
2020-08-19 20:19:56 -0700

    API defined

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const fs = require("fs");
+const express = require("express");
+const app = express();
 const words = require("./dat/wordList.json");
-const smallWords = require("./dat/smallWordList.json");
 let trieDef = require("./comp/Trie");
 
-console.log("nagdi bai");
-
+const PORT = 3003;
 let trie = new trieDef();
 
 words.forEach((word) => trie.insert(word));
-console.log("" + trie.search("tea"));
-const result = trie.findAnagrams("listen");
-console.log("" + result);
+
+app.get("/", function (req, res) {
+  if (!req.params.word) return res.send("Please send a word");
+  let word = req.query.word;
+  res.send(word === undefined ? [] : trie.findAnagrams(word));
+});
+
+app.listen(PORT, () => {
+  console.log(`Anagram Solver listening at http://sanved.com/anagram:${PORT}`);
+});
