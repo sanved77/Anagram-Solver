7cfc70921143a252380c4482c08a48971b5d7fba
sanved77 <sanved77@gmail.com>
2020-08-21 17:53:58 -0700

    Fixed CORS issues, allowed all

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ words.forEach((word) => trie.insert(word));
 
 app.use(cors());
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
+
+// });
+
+app.get("/", function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
   res.setHeader("Access-Control-Allow-Credentials", true);
-});
-
-app.get("/", function (req, res) {
   let word = req.query.word;
   if (req.query.word === undefined) return res.send("Please send a word");
   if (/\d/.test(word)) return res.send("Numbers not allowed");
