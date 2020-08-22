480d3a15a53c0f65774c9ca19b7a1a00c18c9912
sanved77 <sanved77@gmail.com>
2020-08-21 17:49:20 -0700

    CORS headers added

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ words.forEach((word) => trie.insert(word));
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+});
+
 app.get("/", function (req, res) {
   let word = req.query.word;
   if (req.query.word === undefined) return res.send("Please send a word");
